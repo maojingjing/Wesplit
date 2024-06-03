35e41b19fc3184d35344a6db5a92213a2a639bbf
jinging mao <mjj0131@gmail.com>
2024-06-02 20:48:57 -0700

    Initial Commit

---
+++ Wesplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wesplit
+//
+//  Created by Jingjing Mao on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wesplit/WesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WesplitApp.swift
+//  Wesplit
+//
+//  Created by Jingjing Mao on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
